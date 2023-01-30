a48a8b324e54ff4cf70af8ea978cfd99e1fd2579
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-01-30 19:52:43 +0300

    update Restaurant model

--- TechMasters-Homework6/Modules/Homepage/Model/Restaurant.swift
+++ TechMasters-Homework6/Modules/Homepage/Model/Restaurant.swift
@@ -10,4 +10,8 @@ import UIKit
 struct Restaurant{
     var name: String!
     var imageName: String!
+    var hasPhoto: Bool? = false
+    var rating: Double?
+    var commentCount: Int?
+    var isMudavim: Bool
 }
